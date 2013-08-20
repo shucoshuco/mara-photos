5e57163343393cb4e77fa4d32ab4e11d64578f3a
shucoshuco <fgpanda@hotmail.com>
2013-08-20 15:26:26 +0200

    Add behaviour of results

--- src/html/lib/main.js
+++ src/html/lib/main.js
@@ -1,54 +1,54 @@
 var app = angular.module("maraPhotos", []);
 
 var n = 0;
-var baseDate = '20121123';
+var baseDate = Date.parse('Nov 23th, 2012');
 var error = 0;
 var images = [
 	{
 		name: "img/Chrysanthemum.jpg",
-		takenDate: "",
+		takenDate: Date.parse("Dec 25th, 2012"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Desert.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Hydrangeas.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Jellyfish.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Koala.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Lighthouse.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Penguins.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	},
 	{
 		name: "img/Tulips.jpg",
-		takenDate: "",
+		takenDate: Date.parse("March 12, 2013"),
 		error: 0,
 		done: false
 	}
@@ -59,11 +59,32 @@ app.controller("PhotoController", function($scope) {
 
 	$scope.images = images;
 	$scope.current = n;
-	$scope.input = {months: "1", dates: "2"};
+	$scope.input = {months: 1, days: 2};
+	$scope.selection = {};
+	$scope.result = {};
 	$scope.error = 0;
 
-	$scope.updateSummary = function(n) {
+	$scope.updatePhoto = function(n) {
 		$scope.summary = (n + 1) + ' / ' + images.length;
+		if (images[n].done) {
+			$scope.input.months = images[n].selection.months;
+			$scope.input.days = images[n].selection.days;
+			$scope.formEnabled = "disabled";
+
+			$scope.result.date = images[n].takenDate;
+			$scope.result.months = images[n].result.months;
+			$scope.result.days = images[n].result.days;
+			$scope.result.diff = images[n].result.diff; 
+		} else {
+			$scope.input.months = ""
+			$scope.input.days = ""
+			$scope.formEnabled = "false";
+
+			$scope.result.date = "-";
+			$scope.result.months = "-";
+			$scope.result.days = "-";
+			$scope.result.diff = "-";
+		}
 	}
 
 	$scope.getBackgroundImage = function(n) {
@@ -71,14 +92,19 @@ app.controller("PhotoController", function($scope) {
 		return img;
 	}
 
-	$scope.updateSummary($scope.current);
+	$scope.updatePhoto($scope.current);
 })
 
 app.directive("backwardable", function() {
 	return {
 		link: function(scope, element, attrs) {
+
+			var canGoBackward = function(value) {
+				return value > 0;
+			};
+
 			element.bind("mouseenter", function() {
-				if (scope.current > 0)
+				if (canGoBackward(scope.current))
 					element.addClass("backwardShown");
 			});
 
@@ -87,10 +113,10 @@ app.directive("backwardable", function() {
 			});
 
 			element.bind("click", function() {
-				if (scope.current > 0) {
+				if (canGoBackward(scope.current)) {
 					scope.current--;
 					scope.$apply(attrs.backwardable);
-					if (scope.current == 0)
+					if (!canGoBackward(scope.current))
 						element.removeClass("backwardShown")
 				}
 			});
@@ -101,8 +127,14 @@ app.directive("backwardable", function() {
 app.directive("forwardable", function() {
 	return {
 		link: function(scope, element, attrs) {
+		
+			var canGoForward = function(value, images) {
+				return value < images.length - 1
+					&& images[value].done;
+			};
+
 			element.bind("mouseenter", function() {
-				if (scope.current < scope.images.length - 1)
+				if (canGoForward(scope.current, scope.images))
 					element.addClass("forwardShown");
 			});
 
@@ -111,14 +143,51 @@ app.directive("forwardable", function() {
 			});
 
 			element.bind("click", function() {
-				if (scope.current < scope.images.length - 1) {
+				
+				if (canGoForward(scope.current, scope.images)) {
 					scope.current++;
 					scope.$apply(attrs.forwardable);
-					if (scope.current == scope.images.length - 1)
+					if (!canGoForward(scope.current, scope.images))
 						element.removeClass("forwardShown");
 				}
-
 			});
 		}
 	}
 })
+
+app.directive("validate", function($filter) {
+	return {
+		link: function(scope, element, attrs) {
+			element.bind("click", function() {
+
+				var image = scope.images[scope.current];
+
+				var ref = image.takenDate;
+
+				var sel = baseDate.clone();
+				sel.addMonths(scope.input.months);
+				sel.addDays(scope.input.days);
+
+				var doubleDiff = Math.abs(sel.getTime() - ref.getTime()) / (1000 * 60 * 60 * 24);
+				var diff = Math.floor(doubleDiff);
+				console.log(diff);
+
+				image.done = true;
+				image.selection = {
+					months: scope.input.months,
+					days: scope.input.days
+				};
+				image.result = {
+					months: scope.input.months,
+					days: scope.input.days,
+					diff: diff
+				};
+
+				scope.error += diff;
+
+				scope.$apply(attrs.validate);
+
+			});
+		}
+	}
+});
