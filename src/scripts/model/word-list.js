78612e6e6df19cb4a3dc4e868715022d97eced57
FelixPandavenes <felix.pandavenes@medianet.es>
2015-08-17 23:52:51 +0200

    Cambios archivos

--- src/scripts/model/word-list.js
+++ src/scripts/model/word-list.js
@@ -1,443 +1,73 @@
-﻿
-var words = [
-	{
-		name: "media/words/abre.mp3",
-		word: ["Abre"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/abu.mp3",
+﻿		word: ["Abre"],
 		word: ["Abu"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/adios.mp3",
 		word: ["Adiós"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/agua.mp3",
 		word: ["Agua"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/alli.mp3",
 		word: ["Allí"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/alta.mp3",
 		word: ["Alta"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/alvaro.mp3",
 		word: ["Álvaro"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/ana.mp3",
 		word: ["Ana"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/ascensor.mp3",
 		word: ["Ascensor"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/autobus.mp3",
 		word: ["Autobús"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/avion.mp3",
 		word: ["Avión"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/ballena.mp3",
 		word: ["Ballena"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/balon.mp3",
 		word: ["Balón"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/bebe.mp3",
 		word: ["Bebé"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/beee.mp3",
 		word: ["Beee", "Oveja"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/bien.mp3",
 		word: ["Bien"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/camion.mp3",
 		word: ["Camión"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/cereales.mp3",
 		word: ["Cereales"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/ciao.mp3",
 		word: ["Ciao"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/coche.mp3",
 		word: ["Coche"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/cocodrilo.mp3",
 		word: ["Cocodrilo"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/cua.mp3",
 		word: ["Cua-cua", "Pato"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/dientes.mp3",
 		word: ["Dientes"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/dormir.mp3",
 		word: ["Dormir"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/eli.mp3",
 		word: ["Eli"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/estrella.mp3",
 		word: ["Estrella"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/felix.mp3",
 		word: ["Félix"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/gracias.mp3",
 		word: ["Gracias"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/guapa.mp3",
 		word: ["Guapa"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/guau.mp3",
 		word: ["Guau-guau", "Perro"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/hala.mp3",
 		word: ["Hala"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/hasta_luego.mp3",
 		word: ["Hasta luego"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/hector.mp3",
 		word: ["Héctor"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/hola.mp3",
 		word: ["Hola"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/ines.mp3",
 		word: ["Inés"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/irene.mp3",
 		word: ["Irene"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/isabel.mp3",
 		word: ["Isabel"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/kikiriki.mp3",
 		word: ["Kikiriki", "Gallo"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/lapiz.mp3",
 		word: ["Lápiz"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/luz.mp3",
 		word: ["Luz"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/magdalenas.mp3",
 		word: ["Magdalenas"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/mama.mp3",
 		word: ["Mamá"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/mara.mp3",
 		word: ["Mara"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/miau.mp3",
 		word: ["Miau", "Gato"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/moto.mp3",
 		word: ["Moto"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/mucho.mp3",
 		word: ["Mucho"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/muuuu.mp3",
 		word: ["Muuuu", "Vaca"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/muy_bien.mp3",
 		word: ["Muy bien"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/nariz.mp3",
 		word: ["Nariz"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/no.mp3",
 		word: ["No"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/oink.mp3",
 		word: ["Oink", "Cerdo"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/oso.mp3",
 		word: ["Oso"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/oye.mp3",
 		word: ["Oye"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pajaroto.mp3",
 		word: ["Pajaroto"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/papa.mp3",
 		word: ["Papá"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pato.mp3",
 		word: ["Pato"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/petit.mp3",
 		word: ["Petit"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pez.mp3",
 		word: ["Pez"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pie.mp3",
 		word: ["Pie"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pipi.mp3",
 		word: ["Pipí"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pocoyo.mp3",
 		word: ["Pocoyó"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/pulpo.mp3",
 		word: ["Pulpo"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/rana.mp3",
 		word: ["Rana"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/si.mp3",
 		word: ["Si"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/taxi.mp3",
 		word: ["Taxi"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/tesoro.mp3",
 		word: ["Tesoro"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/teta.mp3",
 		word: ["Teta"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/tia.mp3",
 		word: ["Tía"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/tractor.mp3",
 		word: ["Tractor"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/vamos.mp3",
 		word: ["Vamos"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/vaya_cagada.mp3",
 		word: ["Vaya cagada"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/yoghurt.mp3",
 		word: ["Yoghurt"],
-		ok: false,
-		done: false
-	},
-	{
-		name: "media/words/zapatos.mp3",
 		word: ["Zapatos"],
-		ok: false,
-		done: false
-	}
-]
-
-var audios = false;
